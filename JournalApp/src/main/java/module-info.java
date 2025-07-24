65b68c6d21a5383a65ed95fdf8eb887703b50175
rilen-G <gawatrilen123@gmail.com>
2025-07-25 02:49:24 +0800

    Delete JournalApp directory

--- JournalApp/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.journalapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.journalapp to javafx.fxml;
-    exports com.example.journalapp;
-}
